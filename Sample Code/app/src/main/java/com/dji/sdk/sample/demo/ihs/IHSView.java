4e09475b0345ea94ff7ae2c18eaab91dea169695
Rory Hayes <roryohayes@gmail.com>
2017-05-29 23:37:37 -0700

    add hotpoint SDK calls

--- Sample Code/app/src/main/java/com/dji/sdk/sample/demo/ihs/IHSView.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/demo/ihs/IHSView.java
@@ -17,12 +17,19 @@ import com.dji.sdk.sample.internal.view.PresentableView;
 
 import dji.common.camera.SettingsDefinitions;
 import dji.common.error.DJIError;
+import dji.common.flightcontroller.LocationCoordinate3D;
+import dji.common.mission.hotpoint.HotpointHeading;
+import dji.common.mission.hotpoint.HotpointMission;
+import dji.common.mission.hotpoint.HotpointStartPoint;
+import dji.common.model.LocationCoordinate2D;
 import dji.common.util.CommonCallbacks;
 import dji.keysdk.KeyManager;
 import dji.keysdk.callback.ActionCallback;
 import dji.keysdk.callback.GetCallback;
 import dji.keysdk.callback.SetCallback;
 import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.mission.MissionControl;
+import dji.sdk.mission.hotpoint.HotpointMissionOperator;
 import dji.sdk.products.Aircraft;
 
 /**
@@ -36,7 +43,9 @@ public class IHSView extends LinearLayout implements PresentableView {
     private Button circleBtn;
 
     private FlightController flightController;
+    private HotpointMissionOperator hotpointMissionOperator;
 
+    // we'll use this basic callback for anything which might throw and error, so we can log it to examine later
     private CommonCallbacks.CompletionCallback logCallback = new CommonCallbacks.CompletionCallback() {
         @Override
         public void onResult(DJIError djiError) {
@@ -58,7 +67,9 @@ public class IHSView extends LinearLayout implements PresentableView {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+        // initialize our SDK controller and operator objects
         flightController = ((Aircraft) DJISampleApplication.getProductInstance()).getFlightController();
+        hotpointMissionOperator = MissionControl.getInstance().getHotpointMissionOperator();
         setUpListeners();
     }
 
@@ -68,28 +79,53 @@ public class IHSView extends LinearLayout implements PresentableView {
     }
 
     private void setUpListeners() {
+
+        // this listener is triggered when the "take off" button is clicked
         takeOffBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                // tell the flight controller to take off!
                 flightController.startTakeoff(logCallback);
             }
         });
+
+        // this listener is triggered when the "land" button is clicked
         landBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                // tell the flight controller to start landing!
                 flightController.startLanding(logCallback);
             }
         });
+
+        // this listener is triggered when the "return home" button is clicked
         returnHomeBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                // tell the flight controller to start going home!
                 flightController.startGoHome(logCallback);
             }
         });
+
+        // this listener is triggered when the "circle" button is clicked
         circleBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // here's where we do our stuff!
+                // find our current location.  we'll center the circle here.
+                LocationCoordinate3D droneLocation = flightController.getState().getAircraftLocation();
+
+                // create a mission, using the lat/long of our current location and some other values we choose.
+                HotpointMission mission = new HotpointMission(
+                        new LocationCoordinate2D(droneLocation.getLatitude(), droneLocation.getLongitude()), // 2D point to circle around
+                        5, // altitude in meters (~16ft)
+                        5, // radius of circle in meters (~25ft)
+                        18, // angular velocity in degrees per second (full rotation in ~20 seconds)
+                        false, // move clockwise?
+                        HotpointStartPoint.NORTH, // where should the drone start to traverse the circle?
+                        HotpointHeading.TOWARDS_HOT_POINT // which way should the drone face while circling?
+                );
+                // start the mission!
+                hotpointMissionOperator.startMission(mission, logCallback);
             }
         });
     }
@@ -101,6 +137,7 @@ public class IHSView extends LinearLayout implements PresentableView {
 
         layoutInflater.inflate(R.layout.view_ihs, this, true);
 
+        // obtain a reference to the buttons we defined in the view .xml
         takeOffBtn = (Button) findViewById(R.id.ihs_take_off_btn);
         landBtn = (Button) findViewById(R.id.ihs_land_btn);
         returnHomeBtn = (Button) findViewById(R.id.ihs_return_home_btn);
