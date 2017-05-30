987df9e01c4ad828efb28baed3963b4fe795d223
Rory Hayes <roryohayes@gmail.com>
2017-05-29 23:20:00 -0700

    add the flightcontroller SDK calls

--- Sample Code/app/src/main/java/com/dji/sdk/sample/demo/ihs/IHSView.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/demo/ihs/IHSView.java
@@ -3,6 +3,7 @@ package com.dji.sdk.sample.demo.ihs;
 import android.app.Service;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +11,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.dji.sdk.sample.R;
+import com.dji.sdk.sample.internal.controller.DJISampleApplication;
 import com.dji.sdk.sample.internal.utils.ToastUtils;
 import com.dji.sdk.sample.internal.view.PresentableView;
 
 import dji.common.camera.SettingsDefinitions;
 import dji.common.error.DJIError;
+import dji.common.util.CommonCallbacks;
 import dji.keysdk.KeyManager;
 import dji.keysdk.callback.ActionCallback;
 import dji.keysdk.callback.GetCallback;
 import dji.keysdk.callback.SetCallback;
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
 
 /**
  * Class for basic manager view in mission manager
@@ -29,6 +34,15 @@ public class IHSView extends LinearLayout implements PresentableView {
     private Button landBtn;
     private Button returnHomeBtn;
     private Button circleBtn;
+
+    private FlightController flightController;
+
+    private CommonCallbacks.CompletionCallback logCallback = new CommonCallbacks.CompletionCallback() {
+        @Override
+        public void onResult(DJIError djiError) {
+            if (djiError != null) Log.e("IHS Error", djiError.toString());
+        }
+    };
 
     public IHSView(Context context) {
         super(context);
@@ -44,6 +58,7 @@ public class IHSView extends LinearLayout implements PresentableView {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+        flightController = ((Aircraft) DJISampleApplication.getProductInstance()).getFlightController();
         setUpListeners();
     }
 
@@ -56,19 +71,19 @@ public class IHSView extends LinearLayout implements PresentableView {
         takeOffBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // here's where we do our stuff!
+                flightController.startTakeoff(logCallback);
             }
         });
         landBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // here's where we do our stuff!
+                flightController.startLanding(logCallback);
             }
         });
         returnHomeBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // here's where we do our stuff!
+                flightController.startGoHome(logCallback);
             }
         });
         circleBtn.setOnClickListener(new OnClickListener() {
